321d7d8d4f5c0ee2723ae2018de3e627785df40e
Rubel Amin <bagha.vai100@gmail.com>
2021-11-25 21:34:29 +0600

    logo placement

--- temp/App.04586683.js
+++ temp/App.04586683.js
@@ -75582,7 +75582,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34939" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38175" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

8368bca8371484facba6f19891084248bb372d38
Rubel Amin <bagha.vai100@gmail.com>
2021-12-17 15:53:53 +0600

    file upload

--- temp/App.04586683.js
+++ temp/App.04586683.js
@@ -73263,7 +73263,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34381" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41187" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
